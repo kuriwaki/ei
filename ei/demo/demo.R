58007017ba2650736d26b1ffe0e7601042145be3
tercer <jhonaker@iq.harvard.edu>
2014-09-07 18:05:03 -0400

    removing folder copy at higher path

--- ei/demo/demo.R
+++
@@ -1,7 +0,0 @@
-library(ei)
-data(sample)
-formula = t ~ x
-dbuf = ei(formula=formula,total="n",data=sample)
-summary(dbuf)
-eiread(dbuf, "betab", "betaw")
-plot(dbuf, "tomog", "betab", "betaw", "xtfit")
